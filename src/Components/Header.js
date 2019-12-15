fdca7ab6484b800a4d5e94b6abefc95ada1cf576
corgi93 <gurwls714@naver.com>
2019-12-16 01:23:02 +0900

    [노마드 clone]개발 환경 세팅

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default ()=> (
+<header>
+    <ul className=".nav">
+        <li><a href="/">Movies</a></li>
+        <li><a href="/tv">tv</a></li>
+        <li><a href="/search">Search</a></li>
+    </ul>
+</header>
+)
---
+++ src/Components/Router.js
@@ -0,0 +1,20 @@
+import React from "react";
+// 우선 HashRouter을 사용해봄
+// import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import Home from "Routes/Home";
+import TV from "Routes/TV";
+import Search from "Routes/Search";
+
+export default () => (
+    <Router>
+        <Switch>
+            <Route path="/" exact component={ Home }/>
+            <Route path="/tv" exact component={ TV } />
+            <Route path="/search" exact component={ Search } />
+
+            {/* 리액트에서 Redirect */}
+            <Redirect from="*" to="/" />
+        </Switch>
+    </Router>
+);
