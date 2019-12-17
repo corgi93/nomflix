27efdd3fc8449f0719166853a52962ae6045f324
Hyeokjin Jang <gurwls714@gmail.com>
2019-12-17 18:06:53 +0900

    [노마드 clone] 라우터에 Detail 추가
    Detail 컴포넌트를 참조하지만 각각의  url을 가집니다

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,22 +1,32 @@
 import React from "react";
 // 우선 HashRouter을 사용해봄
 // import { HashRouter as Router, Route } from "react-router-dom";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
-import Header from "Components/Header"
+import Detail from "Routes/Detail";
+import Header from "Components/Header";
 
 export default () => (
-    <Router>
-        <Header />
-            <Switch>
-                <Route path="/" exact component={ Home }/>
-                <Route path="/tv" exact component={ TV } />
-                <Route path="/search" exact component={ Search } />
+  <Router>
+    <Header />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" exact component={TV} />
+      <Route path="/search" exact component={Search} />
 
-                {/* 리액트에서 Redirect */}
-                <Redirect from="*" to="/" />
-            </Switch>
-    </Router>
+      {/* Detail. 참조 컴포넌트는 Detail로 같지만 url이 다르다 */}
+      <Route path="/movie/:id" component={Detail} />
+      <Route path="/show/:id" component={Detail} />
+
+      {/* 리액트에서 Redirect */}
+      <Redirect from="*" to="/" />
+    </Switch>
+  </Router>
 );
