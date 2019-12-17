eaad00b5f00060a44d04bff2cbcbd29b8d166351
Hyeokjin Jang <gurwls714@gmail.com>
2019-12-17 17:25:51 +0900

    [노마드 clone] api router 오타 수정
    오타 수정했습니다.

--- src/api.js
+++ src/api.js
@@ -8,44 +8,44 @@ const api = axios.create({
   }
 });
 
-
 /* 
 tv, movie에 대한 url을 위의 api와 각각의 api object에서 즉시 실행하게하면
 매우 clean하게 짤 수 있다!!!!
 */
 export const movieApi = {
-    nowPlaying : ()=> api.get("movie/now_playing"),
-    upcoming: () => api.get("movie/upcoming"),
-    popular : () => api.get("movie/popular"),
-    // movie 디테일
-    movieDetail : (id) => api.get(`movie/${id}`, {
-        params : {
-            append_to_response : "videos"
-        }
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  // movie 디테일
+  movieDetail: id =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos"
+      }
     }),
-    search : term => api.get("search/movie", {
-        params :{
-            query : encodeURIComponent(term)
-        }
+  search: term =>
+    api.get("search/movie", {
+      params: {
+        query: encodeURIComponent(term)
+      }
     })
 };
 
 export const tvApi = {
-    topRated : () => api.get("tv/top"),
-    popular : ()=> api.get("tv/popular"),
-    airingToday : () => api.get("tv/airing_today"),
-    // tv 디테일 -  append_to_response를 이용해 img나 video를 가져오기
-    showDetail: (id) => api.get(`tv/${id}`, {
-        params: {
-            append_to_response : "videos"
-        }
-    }), 
-    
-    search : term => api.get("search/tv", {
-        params: {
-            query : encodeURIComponent(term)
-        }
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  // tv 디테일 -  append_to_response를 이용해 img나 video를 가져오기
+  showDetail: id =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos"
+      }
+    }),
+  search: term =>
+    api.get("search/tv", {
+      params: {
+        query: encodeURIComponent(term)
+      }
     })
-}
-
-export default api;
+};
